cfdd4b9748851c1fa991a3f4858760611779b491
Steven Albers <stevenalbers@gmail.com>
2026-02-07 16:34:56 -0800

    fix: use correct repo base

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: 'stevenalbers/bread-helper'
+  base: 'bread-helper'
 })
